85979b2a6aff27336c2e156495957e4965cd8b72
efea <astarlioglu@outlook.com>
2023-03-30 11:40:17 +0300

    initial commit for part5

---
+++ src/components/Notification.js
@@ -0,0 +1,21 @@
+const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    if(!message.includes("Information")){
+    return (
+      <div className='successNotification'>
+        {message}
+      </div>
+    )
+    }
+    return (
+      <div className='errorNotification'>
+        {message}
+      </div>
+    )
+    
+  }
+  
+  export default Notification
